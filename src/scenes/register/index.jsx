97667d9d79a47e8ae054ad7f871c0f33316147e1
Mayank Virole <mvirole@argusoft.com>
2023-07-08 14:59:41 +0530

    create register and login component

--- src/scenes/register/index.jsx
+++ src/scenes/register/index.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles.css";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login">
+      <div class="row d-flex justify-content-center align-items-center h-100 ">
+        <div class="col-md-9 col-lg-6 col-xl-5">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+            class="img-fluid"
+            alt="Sample"
+          />
+        </div>
+        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+          <form onSubmit={handleSubmit}>
+            <h2>Event Management System</h2>
+            <h3> Register</h3>
+            <div class="form-outline mb-4">
+              <input
+                type="username"
+                class="form-control form-control-lg"
+                placeholder="Enter your username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+
+            <div class="form-outline mb-4">
+              <input
+                type="email"
+                class="form-control form-control-lg"
+                placeholder="Enter a valid email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+
+            <div class="form-outline mb-3">
+              <input
+                type="password"
+                class="form-control form-control-lg"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
+            <div class="text-center text-lg-start mt-4 pt-2">
+              <button
+                type="button"
+                class="btn btn-primary btn-lg"
+                style={{
+                  paddingLeft: "2.5rem",
+                  paddingRight: "2.5rem",
+                  marginBottom: "0.5rem",
+                }}
+              >
+                Register
+              </button>
+              <p class="medium fw-bold mt-2 pt-1 mb-0">
+                Already registered ? <Link to="/login">Login</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
