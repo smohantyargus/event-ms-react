442869a2c9229a3acee46395a569d36abf5fde2d
smohantyargus <smohanty@argusoft.com>
2023-07-10 16:51:03 +0530

    delete toast

--- src/scenes/single-event/index.jsx
+++ src/scenes/single-event/index.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import "./styles.css";
+import { toast } from "react-toastify";
 
 const Event = () => {
   const [event, setEvent] = useState({});
@@ -21,6 +22,16 @@ const Event = () => {
       .delete(`http://localhost:9090/deleteEvent/${id}`)
       .then((response) => {
         navigate("/events");
+        toast.warning("Event Deleted!", {
+          position: "bottom-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
         console.log(response.data);
       })
       .catch((error) => {
