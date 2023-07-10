81d65a70edec10793e6c1a5e3643ca8a34f7f79e
smohantyargus <smohanty@argusoft.com>
2023-07-10 17:13:09 +0530

    delete post button

--- src/scenes/single-event/index.jsx
+++ src/scenes/single-event/index.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import "./styles.css";
 import description from "../../icons/pinned-notes.png";
@@ -7,11 +7,16 @@ import address from "../../icons/map.png";
 import date from "../../icons/schedule.png";
 import link from "../../icons/link.png";
 import { toast } from "react-toastify";
+import UserContext from "context/user/UserContext";
 
 const Event = () => {
   const [event, setEvent] = useState({});
   const navigate = useNavigate();
   const { id } = useParams();
+
+  const { user } = useContext(UserContext);
+  const adminAuth = user.role === "admin";
+
   useEffect(() => {
     axios
       .get(`http://localhost:9090/event/${id}`)
@@ -49,34 +54,36 @@ const Event = () => {
       <h1>{event.eventTitle}</h1>
       <section>
         <p className="desc-block">
-        <img src= {description} alt="description" className="desc-img"/>
-        <span className="desc">
-          {event.eventDescription}
-        </span>
+          <img src={description} alt="description" className="desc-img" />
+          <span className="desc">{event.eventDescription}</span>
         </p>
-          <br></br>
-          <img  className= "address-img" src={address} alt="address"/> {event.eventLocation}
-          <br></br>
-          <img  className= "date-img" src={date} alt="date"/> {event.eventStartTime} - {event.eventEndTime}
-          <br></br>
-          <img  className="link-img" src={link} alt="link"/><a href={event.eventLink}>Open Link</a>
-        
+        <br></br>
+        <img className="address-img" src={address} alt="address" />{" "}
+        {event.eventLocation}
+        <br></br>
+        <img className="date-img" src={date} alt="date" />{" "}
+        {event.eventStartTime} - {event.eventEndTime}
+        <br></br>
+        <img className="link-img" src={link} alt="link" />
+        <a href={event.eventLink}>Open Link</a>
       </section>
       {event.eventAddInfo && <p>Additional info : {event.eventAddInfo}</p>}
-      <button
-        className="btn btn-primary"
-        onClick={handleDelete}
-        style={{
-          backgroundColor: "#802f59",
-          borderColor: "#802f59",
-        }}
-      >
-        Delete Post
-      </button>
+      {adminAuth ? (
+        <button
+          className="btn btn-primary"
+          onClick={handleDelete}
+          style={{
+            backgroundColor: "#802f59",
+            borderColor: "#802f59",
+          }}
+        >
+          Delete Post
+        </button>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
-
-
 
 export default Event;
