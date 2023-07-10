c8039c9ce48d29df300972048eb327aff5704d83
Mayank Virole <mvirole@argusoft.com>
2023-07-10 14:34:06 +0530

    create ui for displaying all events

--- src/scenes/events/index.jsx
+++ src/scenes/events/index.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 
 const Events = () => {
   const [events, setEvents] = useState([]);
-  const [selectedEvent, setSelectedEvent] = useState(null);
   const navigate = useNavigate();
   const getAllEvents = () => {
     axios
@@ -26,15 +25,30 @@ const Events = () => {
   return (
     <div className="events-container">
       <h1>Events</h1>
-      <ul className="event-main">  
-        { events.map((event) => (
+      <ul className="event-main ag-format-container">
+        <div className="ag-courses_box">
+          {events.map((event) => (
             <li key={event.id} onClick={() => handleClick(event)}>
-              <div className="event" >
-                <h3>{event.eventTitle}</h3>
-                <p>{event.eventDescription.substring(0,25)}</p>
+              <div className="event ag-courses_item">
+                {/* <h3>{event.eventTitle}</h3>
+                <p>{event.eventDescription.substring(0,25)}</p> */}
+                <a href="#" class="ag-courses-item_link">
+                  <div class="ag-courses-item_bg"></div>
+
+                  <div class="ag-courses-item_title">
+                    {event.eventTitle}
+                  </div>
+
+                  <div class="ag-courses-item_date-box">
+                    Start:
+                    <span class="ag-courses-item_date">{event.eventStartTime}</span>
+                    
+                  </div>
+                </a>
               </div>
             </li>
           ))}
+        </div>
       </ul>
     </div>
   );
