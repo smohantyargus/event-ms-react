78bf6da515e19f08a8964d0df8d2e8e7e0e82976
smohantyargus <smohanty@argusoft.com>
2023-07-12 14:32:34 +0530

    single event page

--- src/scenes/single-event/index.jsx
+++ src/scenes/single-event/index.jsx
@@ -2,11 +2,11 @@ import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import "./styles.css";
-import description from "../../icons/pinned-notes.png";
-import address from "../../icons/map.png";
-import date from "../../icons/schedule.png";
-import link from "../../icons/link.png";
-import info from "../../icons/info.png";
+// import description from "../../icons/pinned-notes.png";
+// import address from "../../icons/map.png";
+// import date from "../../icons/schedule.png";
+// import link from "../../icons/link.png";
+// import info from "../../icons/info.png";
 import { toast } from "react-toastify";
 import UserContext from "context/user/UserContext";
 
@@ -50,6 +50,27 @@ const Event = () => {
       });
   };
 
+  const handleInterested = (e) => {
+    e.preventDefault();
+    const eventId = id;
+    const userId = user.userId;
+    axios
+      .get(`http://localhost:9090/attend/${eventId}/${userId}`)
+      .then((res) => {
+        toast.success(`Thank you for registering!`, {
+          position: "bottom-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <>
       <div className="event-container text-center">
@@ -74,6 +95,7 @@ const Event = () => {
           <div className="event-row-3-middle event-box shadow col">
             <button
               className="btn btn-primary btn-lg"
+              onClick={handleInterested}
               style={{
                 backgroundColor: "#802f59",
                 borderColor: "#802f59",
@@ -103,49 +125,6 @@ const Event = () => {
           <></>
         )}
       </div>
-      {/* <div className="single-event">
-        <h1>{event.eventTitle}</h1>
-        <section>
-          <p className="desc-block">
-            <img src={description} alt="description" className="desc-img" />
-            <span className="desc">{event.eventDescription}</span>
-          </p>
-          <p>
-            <img className="address-img" src={address} alt="address" />{" "}
-            {event.eventLocation}
-          </p>
-          <p>
-            <img className="date-img" src={date} alt="date" />{" "}
-            {event.eventStartTime} - {event.eventEndTime}
-            <br></br>
-          </p>
-          <p>
-            <img className="link-img" src={link} alt="link" />
-            <a href={event.eventLink}>Open Link</a>
-          </p>
-          {event.eventAddInfo && (
-            <p>
-              <img className="info-img" src={info} alt="additional info" />{" "}
-              {event.eventAddInfo}
-            </p>
-          )}
-        </section>
-
-        {adminAuth ? (
-          <button
-            className="btn btn-primary"
-            onClick={handleDelete}
-            style={{
-              backgroundColor: "#802f59",
-              borderColor: "#802f59",
-            }}
-          >
-            Delete Post
-          </button>
-        ) : (
-          <></>
-        )}
-      </div> */}
     </>
   );
 };
