04c4390595e35f90b89dd97ab615b1e5e0651261
smohantyargus <smohanty@argusoft.com>
2023-07-12 15:29:46 +0530

    nav ui fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,31 +41,66 @@ const Navbar = () => {
             class="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
+            data-bs-target="#navbarScroll"
+            aria-controls="navbarScroll"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+          <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul
+              class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+              // style="--bs-scroll-height: 100px;"
+            >
               <li class="nav-item">
                 <Link class="nav-link" to="/events">
                   Events
                 </Link>
               </li>
-              <li class="nav-item"></li>
+              {/* <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarScrollingDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Link
+                </a>
+                <ul
+                  class="dropdown-menu"
+                  aria-labelledby="navbarScrollingDropdown"
+                >
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li> */}
             </ul>
+            <form class="d-flex">
+              <button
+                style={{ backgroundColor: "#802f59", borderColor: "#802f59" }}
+                class="btn btn-primary my-2 my-sm-0 d-flex"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </form>
           </div>
-          <button className="btn disabled">Welcome {user.username}</button>
-          <button
-            style={{ backgroundColor: "#802f59", borderColor: "#802f59" }}
-            class="btn btn-primary my-2 my-sm-0"
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
         </div>
       </nav>
     </div>
