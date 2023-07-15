7ddfb08437e1533ebd4d40a011a1751395e730c4
smohantyargus <smohanty@argusoft.com>
2023-07-15 11:46:12 +0530

    register ui

--- src/scenes/register/index.jsx
+++ src/scenes/register/index.jsx
@@ -6,6 +6,8 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import api from "api";
 
+import logo from "../../icons/logo.png";
+
 const Register = () => {
   const [email, setEmail] = useState("");
   // const [username, setUsername] = useState("");
@@ -29,7 +31,6 @@ const Register = () => {
     let data = {
       firstName,
       lastName,
-      // username,
       email,
       password,
       role,
@@ -40,7 +41,7 @@ const Register = () => {
       .then((response) => {
         // Handle the response
         console.log(response.data);
-        localStorage.setItem('token',response.data.token);
+        localStorage.setItem("token", response.data.token);
         navigate("/login");
         toast.success("User Registered!", {
           position: "bottom-center",
@@ -60,95 +61,72 @@ const Register = () => {
   };
 
   return (
-    <div className="login">
-      <div class="row d-flex justify-content-center align-items-center h-100 ">
-        <div class="col-md-9 col-lg-6 col-xl-5">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-            class="img-fluid"
-            alt="Sample"
+    <div className="register-container">
+      <div className="register row shadow">
+        <img src={logo} className="arg-logo" />
+        <form onSubmit={handleSubmit}>
+          <div className="register-header">
+            <h3>Event-MS Register</h3>
+          </div>
+
+          <input
+            type="firstname"
+            class="form-control form-control-lg input"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
           />
-        </div>
-        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-          <form onSubmit={handleSubmit}>
-            <h2>Event Management System</h2>
-            <h3> Register</h3>
-            {/* <div class="form-outline mb-4">
-              <input
-                type="username"
-                class="form-control form-control-lg"
-                placeholder="Enter Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
-            </div> */}
-            <div class="form-outline mb-4">
-              <input
-                type="firstname"
-                class="form-control form-control-lg"
-                placeholder="Enter First Name"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                required
-              />
-            </div>
-            <div class="form-outline mb-4">
-              <input
-                type="lastname"
-                class="form-control form-control-lg"
-                placeholder="Enter Last Name"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                required
-              />
-            </div>
-            <div class="form-outline mb-4">
-              <input
-                type="email"
-                class="form-control form-control-lg"
-                placeholder="Enter Email Address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
 
-            <div class="form-outline mb-3">
-              <input
-                type={showPassword ? "text" : "password"}
-                class="form-control form-control-lg"
-                placeholder="Enter Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <button className="btn mt-2" onClick={toggle}>
-                {showPassword ? "Hide Password" : "Show Password"}
-              </button>
-            </div>
+          <input
+            type="lastname"
+            class="form-control form-control-lg input"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
 
-            <div class="text-center text-lg-start mt-4 pt-2">
-              <button
-                type="button"
-                class="btn btn-primary btn-lg"
-                style={{
-                  paddingLeft: "2.5rem",
-                  paddingRight: "2.5rem",
-                  marginBottom: "0.5rem",
-                  backgroundColor: "#802f59",
-                  borderColor: "#802f59",
-                }}
-                onClick={handleSubmit}
-              >
-                Register
-              </button>
-              <p class="medium fw-bold mt-2 pt-1 mb-0">
-                Already registered ? <Link to="/login">Login</Link>
-              </p>
-            </div>
-          </form>
-        </div>
+          <input
+            type="email"
+            class="form-control form-control-lg input"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <input
+            type={showPassword ? "text" : "password"}
+            class="form-control form-control-lg input"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button className="btn mt-2 input" onClick={toggle}>
+            {showPassword ? "Hide Password" : "Show Password"}
+          </button>
+          <div>
+            <button
+              type="button"
+              class="btn btn-primary btn-lg"
+              style={{
+                paddingLeft: "2.5rem",
+                paddingRight: "2.5rem",
+                marginBottom: "0.5rem",
+                backgroundColor: "#802f59",
+                borderColor: "#802f59",
+              }}
+              onClick={handleSubmit}
+            >
+              Register
+            </button>
+          </div>
+          <p class="mt-4">
+            Already registered ? <Link to="/login">Login</Link>
+          </p>
+        </form>
       </div>
     </div>
   );
