6c008f3db9c30b8178e43f88b878b17d0d38a0de
smohantyargus <smohanty@argusoft.com>
2023-07-15 11:31:41 +0530

    login ui

--- src/scenes/login/index.jsx
+++ src/scenes/login/index.jsx
@@ -7,6 +7,8 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import api from "api";
 
+import logo from "../../icons/logo.png";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -94,67 +96,56 @@ const Login = () => {
   };
 
   return (
-    <div className="login">
-      <div class="row d-flex justify-content-center align-items-center h-100 ">
-        <div class="col-md-9 col-lg-6 col-xl-5">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-            class="img-fluid"
-            alt="s"
+    <div className="login-container">
+      <div class="login row shadow">
+        <img src={logo} className="arg-logo" />
+        <form onSubmit={handleSubmit}>
+          {/* <h2>Event Management System</h2> */}
+          <div className="login-header">
+            <h3>Event-MS Login</h3>
+          </div>
+          <input
+            type="email"
+            id="form3Example3"
+            class="form-control form-control-lg input"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
           />
-        </div>
-        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-          <form onSubmit={handleSubmit}>
-            <h2>Event Management System</h2>
-            <h3> Login</h3>
-            <div class="form-outline mb-4">
-              <input
-                type="email"
-                id="form3Example3"
-                class="form-control form-control-lg"
-                placeholder="Email Address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
 
-            <div class="form-outline mb-3">
-              <input
-                type={showPassword ? "text" : "password"}
-                id="form3Example4"
-                class="form-control form-control-lg"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <button className="btn mt-2" onClick={toggle}>
-                {showPassword ? "Hide Password" : "Show Password"}
-              </button>
-            </div>
-
-            <div class="text-center text-lg-start mt-4 pt-2">
-              <button
-                type="button"
-                class="btn btn-primary btn-lg"
-                style={{
-                  paddingLeft: "2.5rem",
-                  paddingRight: "2.5rem",
-                  marginBottom: "0.5rem",
-                  backgroundColor: "#802f59",
-                  borderColor: "#802f59",
-                }}
-                onClick={handleSubmit}
-              >
-                Login
-              </button>
-              <p class="medium fw-bold mt-2 pt-1 mb-0">
-                Don't have an account? <Link to="/register">Register</Link>
-              </p>
-            </div>
-          </form>
-        </div>
+          <input
+            type={showPassword ? "text" : "password"}
+            id="form3Example4"
+            class="form-control form-control-lg input"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button className="btn mt-2 input" onClick={toggle}>
+            {showPassword ? "Hide Password" : "Show Password"}
+          </button>
+          <div>
+            <button
+              type="button"
+              class="btn btn-primary btn-lg"
+              style={{
+                paddingLeft: "2.5rem",
+                paddingRight: "2.5rem",
+                marginBottom: "0.5rem",
+                backgroundColor: "#802f59",
+                borderColor: "#802f59",
+              }}
+              onClick={handleSubmit}
+            >
+              Login
+            </button>
+          </div>
+          <p class="mt-4">
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
+        </form>
       </div>
     </div>
   );
