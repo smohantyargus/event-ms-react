3194b6413d47c9b714dd634a4802d35d20c06364
smohantyargus <smohanty@argusoft.com>
2023-07-20 13:11:44 +0530

    build

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1/",
+  baseURL: "http://192.1.200.111:8080/api/v1/",
   headers: {
     common: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
