3f41f3b3a8e378a2a85187ff1d8ffa1e53238191
smohantyargus <smohanty@argusoft.com>
2023-07-20 11:35:23 +0530

    api fix jwt

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api/v1/",
-    headers : {
-        common : {
-            'Authorization' : localStorage.getItem('token')
-        }
-    }
+  baseURL: "http://localhost:8080/api/v1/",
+  headers: {
+    common: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  },
 });
 
 export default api;
