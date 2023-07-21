c31b2614d149874151fa41009d8fd94dc7aad792
smohantyargus <smohanty@argusoft.com>
2023-07-21 09:53:15 +0530

    api cors fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.1.200.111:8080/api/v1/",
-  headers: {
-    common: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-  },
+  baseURL: "http://localhost:8080/api/v1/",
+});
+
+api.interceptors.request.use((req) => {
+  if (localStorage.getItem("token")) {
+    req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  }
+
+  return req;
 });
 
 export default api;
